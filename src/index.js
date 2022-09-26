012b66fb832fd5f5067f8c83aa55aaee9e1359c6
vitanv <vitanvlad20@gmail.com>
2022-09-26 21:39:14 +0100

    Added API call

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 
+var from = "EUR";
+var to ="USD";
+var amount = 10;
+var requestURL = 'https://api.exchangerate.host/convert?from='+from+'&to='+to;
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,3 +20,7 @@ root.render(
   </React.StrictMode>
 );
 
+request.onload = function() {
+  var response = request.response;
+  console.log(response.result * amount);
+}
