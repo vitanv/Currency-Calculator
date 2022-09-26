2bb356714507a67d139a5a8c98b7b7fb34722118
vitanv <vitanvlad20@gmail.com>
2022-09-26 21:22:19 +0100

    Initial page layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 export class App extends React.Component{
   render(){
     return (
-      <div className="container">
+      <div className="container" id="wrapper">
+          <h1>Currency Converter</h1>
+          <div className='input'>
+            <input type="number"></input>
+            <p>to</p>
+            <input type="number"></input>
+          </div>
+          <button className='btn-primary'>Convert</button>
+          <span id="result">Placeholder</span>
       </div>
     );
   }
