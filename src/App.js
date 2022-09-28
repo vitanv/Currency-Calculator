94abef5dc7376dc88aaa85598832acfca081f2d7
vitanv <vitanvlad20@gmail.com>
2022-09-28 21:10:42 +0100

    Increased the number of currencies. Made USD and GBP default

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export class App extends React.Component{
     super(props);
     this.state = {
       from:"USD",
-      to:"EUR",
+      to:"GBP",
       rate:1,
       amount:0,
     }
@@ -38,9 +38,19 @@ export class App extends React.Component{
   }
 
   createList = () =>{
-    let list = ["USD","GBP","EUR"];
+    let list = ["AFA", "ALL", "DZD", "AOA", "ARS", "AMD", "AWG", "AUD", "AZN", "BSD", "BHD", "BDT", "BBD", "BYR", "BEF", "BZD",
+              "BMD", "BTN", "BTC", "BOB", "BAM", "BWP", "BRL", "BND", "BGN", "BIF", "KHR", "CAD", "CVE", "KYD", "XOF",
+              "XAF", "XPF", "CLP", "CNY", "COP", "KMF", "CDF", "CRC", "HRK", "CUC", "CZK", "DKK", "DJF", "DOP", "XCD", "EGP",
+              "ERN", "EEK", "ETB", "EUR", "FKP", "FJD", "GMD", "GEL", "DEM", "GHS", "GIP", "GRD", "GTQ", "GNF", "GYD", "HTG",
+              "HNL", "HKD", "HUF", "ISK", "INR", "IDR", "IRR", "IQD", "ILS", "ITL", "JMD", "JPY", "JOD", "KZT", "KES", "KWD",
+              "KGS", "LAK", "LVL", "LBP", "LSL", "LRD", "LYD", "LTL", "MOP", "MKD", "MGA", "MWK", "MYR", "MVR", "MRO", "MUR", 
+              "MXN", "MDL", "MNT", "MAD", "MZM", "MMK", "NAD", "NPR", "ANG", "TWD", "NZD", "NIO", "NGN", "KPW", "NOK", "OMR",
+              "PKR", "PAB", "PGK", "PYG", "PEN", "PHP", "PLN", "QAR", "RON", "RUB", "RWF", "SVC", "WST", "SAR", "RSD", "SCR",
+              "SLL", "SGD", "SKK", "SBD", "SOS", "ZAR", "KRW", "XDR", "LKR", "SHP", "SDG", "SRD", "SZL", "SEK", "CHF", "SYP",
+              "STD", "TJS", "TZS", "THB", "TOP", "TTD", "TND", "TRY", "TMT", "UGX", "UAH", "AED", "UYU", "UZS", "VUV",
+              "VEF", "VND", "YER", "ZMK"];
     let option = [];
-    for(let i = 0; i < 3;i++){
+    for(let i = 0; i < list.length;i++){
       option.push(<option value={list[i]}>{list[i]}</option>)
     }
     return option;
@@ -52,17 +62,19 @@ export class App extends React.Component{
           <h1>Currency Converter</h1>
           <div className='input'>
             <select id="from-currency" onChange={(e) => this.changeFrom(e.target.value)}>
+              <option value = "USD">USD</option>
               {this.createList()} 
             </select>
             <p>to</p>
             
             <select id="to-currency" onChange={(e) => this.changeTo(e.target.value)}>
+              <option value = "GBP">GBP</option>
               {this.createList()}
             </select>
           </div>
           <input type="number" onChange={(e) => this.changeAmount(e.target.value)}></input>
           <button className='btn-primary'>Convert</button>
-          <span id="result">{this.state.rate * this.state.amount} at a rate of {this.state.rate} {this.state.to} to 1 {this.state.from} </span>
+          <span id="result">{this.state.rate * this.state.amount} at a rate of {this.state.rate}  to 1 {this.state.from} </span>
       </div>
     );
   }
